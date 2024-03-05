35b65dff6b184121cff1c1e6d64fab9eea4825e2
Freedom <1358762936@qq.com>
2024-03-05 21:23:39 +0800

    init

---
+++ script/pagination_kor.js
@@ -0,0 +1,515 @@
+const listItem = [
+  {
+    title: "신한운용, 성장지원펀드 운용사로 VIG·IMM 낙점",
+    link: "https://www.sedaily.com/NewsView/29X5NASMD4",
+    date: "2023.11.08",
+    media: "서울경제",
+    lang: "KOR",
+  },
+  {
+    title: "산재기금 2천억 PEF 운용사에 'VIG·IMM인베·도미누스·BNW' 선정",
+    link: "https://news.einfomax.co.kr/news/articleView.html?idxno=4286409",
+    date: "2023.11.02",
+    media: "연합인포맥스",
+    lang: "KOR",
+  },
+  {
+    title: "노란우산공제 2600억 출자 PE 6개사 가닥...VIG·맥쿼리 ‘승기’",
+    link: "https://www.msn.com/ko-kr/news/other/%EB%8B%A8%EB%8F%85-%EB%85%B8%EB%9E%80%EC%9A%B0%EC%82%B0%EA%B3%B5%EC%A0%9C-2600%EC%96%B5-%EC%B6%9C%EC%9E%90-pe-6%EA%B0%9C%EC%82%AC-%EA%B0%80%EB%8B%A5-vig-%EB%A7%A5%EC%BF%BC%EB%A6%AC-%EC%8A%B9%EA%B8%B0/ar-AA1ilyM0",
+    date: "2023.10.18",
+    media: "이데일리",
+    lang: "KOR",
+  },
+  {
+    title: "사학연금, 블라인드펀드 위탁운용사 선정…VIG·IMM·한앤컴퍼니",
+    link: "https://news.einfomax.co.kr/news/articleView.html?idxno=4282186",
+    date: "2023.09.25",
+    media: "연합인포맥스",
+    lang: "KOR",
+  },
+  {
+    title: "'VIG 편입' 프리드라이프, '자산 2.2조' 몸집 불린 까닭은",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202306091547477640105249",
+    date: "2023.06.14",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "VIG가 인수한 본촌치킨, 프랑스 공략 첫발…'파리 1호점' 오픈",
+    link: "https://www.edaily.co.kr/news/read?newsId=01787606635547912&mediaCodeNo=257",
+    date: "2023.03.28",
+    media: "이데일리",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스가 인수한 이스타항공, 국토부 AOC 재취득",
+    link: "https://www.mk.co.kr/news/business/10662799",
+    date: "2023.02.28",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "사모펀드 VIG파트너스 '본촌치킨', 미국현지 '인기 치킨윙 브랜드로'",
+    link: "https://mnb.moneys.co.kr/mnbview.php?no=2023022115032629423",
+    date: "2023.02.22",
+    media: "MoneyS",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 이스타항공 인수 거래 마무리",
+    link: "https://www.mk.co.kr/news/stock/10622588",
+    date: "2023.01.30",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 이스타항공 M&A 거래 종결…1100억원 투입",
+    link: "https://marketinsight.hankyung.com/article/202301300773r",
+    date: "2023.01.30",
+    media: "마켓인사이트",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 저비용 항공사 이스타항공 인수",
+    link: "https://www.mk.co.kr/news/business/10596848",
+    date: "2023.01.06",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "[2022 PE 애뉴얼 리포트] VIG파트너스, 투자·회수 두마리 토끼 잡았다",
+    link: "http://m.thebell.co.kr/m/newsview.asp?svccode=00&newskey=202212260738074220106076",
+    date: "2022.12.29",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 해외LP 러브콜 돋보인 4호 펀드",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202210241129144760105657&svccode=00&page=1&sort=thebell_check_time",
+    date: "2022.10.25",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "VIG가 품은 티젠, '김태리 콤부차'로 쾌속 성장…매출 50%↑기대",
+    link: "https://m.mt.co.kr/renew/view.html?no=2022091416411742779&type=outlink&ref=https%3A%2F%2Fsearch.naver.com",
+    date: "2022.09.15",
+    media: "머니투데이",
+    lang: "KOR",
+  },
+  {
+    title:
+      "[인터뷰] 신창훈 VIG파트너스 부대표 “공감·소통 잘하는 능력, 냉철한 분석력보다 중요하다”",
+    link: "http://economychosun.com/client/news/view.php?boardName=C03&t_num=13613358",
+    date: "2022.09.14",
+    media: "조선비즈",
+    lang: "KOR",
+  },
+  {
+    title: "VIG와 눈맞춤…'블랙핑크 렌즈' 세계서 빛났다",
+    link: "https://www.hankyung.com/economy/article/2022070455141",
+    date: "2022.07.04",
+    media: "한국경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 스마트스코어에 1800억 투자",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202206281031242000101691&svccode=00&page=1&sort=thebell_check_time",
+    date: "2022.06.29",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "[VIG 포트폴리오] 본촌치킨, 코로나 악재 속 미국시장 가능성 엿봤다",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202205040636340420105792&svccode=00&page=1&sort=thebell_check_time",
+    date: "2022.05.06",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, `김태리 콤부차` 티젠 800억에 인수",
+    link: "https://m.mk.co.kr/news/business/10301433",
+    date: "2022.04.26",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 스타비젼 성공적 엑시트",
+    link: "https://marketinsight.hankyung.com/article/202204070080r",
+    date: "2022.04.07",
+    media: "마켓인사이트",
+    lang: "KOR",
+  },
+  {
+    title: "[VIG 포트폴리오] 팜아산, PE 실탄 장전한 노림수",
+    link: "https://dealsite.co.kr/articles/85042",
+    date: "2022.03.29",
+    media: "딜사이트",
+    lang: "KOR",
+  },
+  {
+    title: "주차장·상조…VIG, 해외서 검증된 업종 앞선 투자로 대박",
+    link: "https://www.mk.co.kr/news/stock/10214793",
+    date: "2022.02.10",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 2호 펀드 청산 이어 3호 매각도 '속도'",
+    link: "https://n.news.naver.com/article/015/0004659404?sid=101",
+    date: "2022.02.03",
+    media: "한국경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 바디프랜드 매각 마무리 단계…올해 2곳 투자 예정",
+    link: "https://news.mt.co.kr/mtview.php?no=2022010614064094833",
+    date: "2022.01.07",
+    media: "머니투데이",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 스타비젼 경영권 매각…투자 대비 2배 수익",
+    link: "https://m.mt.co.kr/renew/view.html?no=2022010314063568105&type=outlink&ref=https%3A%2F%2Fsearch.naver.com",
+    date: "2022.01.03",
+    media: "머니투데이",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 투자·회수·펀딩 삼박자 고른 활약",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202112150712563140101430&svccode=00&page=1&sort=thebell_check_time",
+    date: "2021.12.16",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스2호 펀드 청산 '눈앞'…4호 이어 5호도 준비",
+    link: "https://n.news.naver.com/article/016/0001924866?sid=101",
+    date: "2021.12.15",
+    media: "헤럴드경제",
+    lang: "KOR",
+  },
+  {
+    title: "바디프랜드 매각' VIG파트너스, 연이은 투자금 회수 눈길",
+    link: "https://newsis.com/view/?id=NISX20211103_0001637825&cID=15001&pID=15000",
+    date: "2021.11.03",
+    media: "뉴시스",
+    lang: "KOR",
+  },
+  {
+    title: "VIG, 푸디스트 물류 창고 1200억원에 매각",
+    link: "https://www.mk.co.kr/news/business/10038272",
+    date: "2021.09.26",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG, 프리드라이프 자본재조정·소수지분 매각",
+    link: "https://dealsite.co.kr/articles/78217",
+    date: "2021.09.06",
+    media: "딜사이트",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 프리드라이프 지분 10% 마스턴에 매각",
+    link: "https://www.mk.co.kr/news/stock/10015393",
+    date: "2021.09.03",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "사모펀드 `VIG파트너스`, 폐기물社에 1000억 투자",
+    link: "https://www.mk.co.kr/news/stock/10011185",
+    date: "2021.08.31",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 본촌치킨 투자원금 회수 임박",
+    link: "https://dealsite.co.kr/articles/75284",
+    date: "2021.06.18",
+    media: "딜사이트",
+    lang: "KOR",
+  },
+  {
+    title: "쿤달 점찍은 VIG파트너스, 'K-케어' 성장잠재력 주목",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202105141510096160104664&svccode=00&page=1&sort=thebell_check_time",
+    date: "2021.05.17",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "`쿤달샴푸`로 뜬 더스킨팩토리…VIG가 1000억 후반대에 인수",
+    link: "https://www.mk.co.kr/news/stock/9869877",
+    date: "2021.05.13",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG 투자 본촌, K-치킨서 글로벌 브랜드 도약 기대",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202104061233253800108067&svccode=00&page=1&sort=thebell_check_time",
+    date: "2021.04.08",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "VIG 합류한 정연박 변호사, 집단지성 높인다",
+    link: "https://dealsite.co.kr/articles/70675",
+    date: "2021.02.08",
+    media: "딜사이트",
+    lang: "KOR",
+  },
+  {
+    title: "10년 동고동락, VIG 아이콘 등극한 한영기 전무",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202102010901212620108695&svccode=00&page=1&sort=thebell_check_time",
+    date: "2021.02.03",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "난립하던 상조업계 재편 신호탄 쏜 VIG파트너스",
+    link: "https://news.mt.co.kr/mtview.php?no=2021012013474214218",
+    date: "2021.01.21",
+    media: "머니투데이",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 정연박 변호사 전무로 영입",
+    link: "https://www.mk.co.kr/news/stock/9588289",
+    date: "2020.11.02",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title:
+      "[PEF 릴레이 분석 ④VIG파트너스] 4인4색 승부사들…손 댄 기업 모두 ‘밸류업’",
+    link: "http://news.heraldcorp.com/view.php?ud=20200831000469",
+    date: "2020.08.31",
+    media: "헤럴드경제",
+    lang: "KOR",
+  },
+  {
+    title:
+      "[PEF 릴레이 분석 ④VIG파트너스] 줄줄이 ‘대박 펀드’…4호펀드도 순풍에 돛",
+    link: "http://news.heraldcorp.com/view.php?ud=20200831000467",
+    date: "2020.08.31",
+    media: "헤럴드경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG, 동종업계 역발상 M&A 전략 통했다",
+    link: "https://www.hankyung.com/finance/article/2020083170271",
+    date: "2020.08.31",
+    media: "한국경제",
+    lang: "KOR",
+  },
+  {
+    title:
+      "[주목! 이 PEF] 업계 1위 프리드라이프 거머쥔 VIG, 차별화된 '볼트온' 전략 '탁월'",
+    link: "https://marketinsight.hankyung.com/article/202009010884u",
+    date: "2020.08.30",
+    media: "한국경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG, 윈플러스 볼트온으로 사업확장 가속도",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202006290046029900106961&svccode=00&page=1&sort=thebell_check_time",
+    date: "2020.06.30",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 프리드라이프 인수 완료",
+    link: "https://www.mk.co.kr/news/stock/9405140",
+    date: "2020.06.26",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "M&A 격전지 상조업계…VIG파트너스 '큰손' 우뚝",
+    link: "https://www.edaily.co.kr/news/read?newsId=01197206625805328&mediaCodeNo=257&OutLnkChk=Y",
+    date: "2020.06.25",
+    media: "이데일리",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 상조업계 1위 프리드라이프 인수",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202004100650046340106285&svccode=00&page=1&sort=thebell_check_time",
+    date: "2020.04.10",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "신창훈 VIG 부대표, 1세대 PE 주축으로 우뚝",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202001200932559280104265&svccode=00&page=1&sort=thebell_check_time",
+    date: "2020.01.22",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스 4호펀드 결성…목표초과해 9500억원 유치",
+    link: "https://www.mk.co.kr/news/stock/9151291",
+    date: "2020.01.06",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG, '미들마켓 바이아웃 하우스' 위상 확고",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=201912101117426360103232&svccode=00&page=1&sort=thebell_check_time",
+    date: "2019.12.11",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "한화 외식사업부, 1000억원에 VIG파트너스 품으로",
+    link: "https://marketinsight.hankyung.com/article/201911252017u",
+    date: "2019.11.25",
+    media: "한국경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 통합 교육기업 디쉐어 1천650억에 인수",
+    link: "https://news.einfomax.co.kr/news/articleView.html?idxno=4050292",
+    date: "2019.10.01",
+    media: "연합인포맥스",
+    lang: "KOR",
+  },
+  {
+    title: "VIG, 이번에도 잭팟… 500억원 투자한 하이파킹 1,700억원에 매각",
+    link: "https://www.sedaily.com/NewsView/1VN66A5Y08",
+    date: "2019.08.30",
+    media: "서울경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 삼양옵틱스 LK에 매각..1020억 규모",
+    link: "https://www.fnnews.com/news/201906261844560034",
+    date: "2019.06.26",
+    media: "파이낸셜뉴스",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 글로벌 한식 시장 베팅…본촌 인터내셔날 인수",
+    link: "https://www.sedaily.com/NewsView/1S7E1G515Y",
+    date: "2018.11.30",
+    media: "서울경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG가 찍은 '본촌', 저커버그가 찾은 바로 그 치킨",
+    link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=201811300100051810003172&svccode=00&page=1&sort=thebell_check_time",
+    date: "2018.11.30",
+    media: "더벨",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 애누리닷컴 1000억원에 코리아센터에 매각",
+    link: "https://marketinsight.hankyung.com/article/201806082762u",
+    date: "2018.06.08",
+    media: "한국경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 뷰티 콘택트렌즈 전문기업 '스타비젼' 1375억 투자",
+    link: "http://www.investchosun.com/site/data/html_dir/2018/06/05/2018060586003.html",
+    date: "2018.06.05",
+    media: "인베스트조선",
+    lang: "KOR",
+  },
+  {
+    title: "VIG, 식자재유통사 윈플러스 인수",
+    link: "https://www.mk.co.kr/news/stock/8198504",
+    date: "2018.02.19",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스, 7000억 3호 블라인펀드 결성 완료",
+    link: "http://www.investchosun.com/site/data/html_dir/2017/02/14/2017021486014.html",
+    date: "2017.02.14",
+    media: "인베스트조선",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스 5100억원 규모 3호펀드 결성…좋은상조 인수",
+    link: "http://www.investchosun.com/site/data/html_dir/2016/09/29/2016092986014.html",
+    date: "2016.09.29",
+    media: "인베스트조선",
+    lang: "KOR",
+  },
+  {
+    title: "고령화시대 발맞춘 VIG파트너스, `좋은상조` 650억에 인수",
+    link: "https://www.mk.co.kr/news/special-edition/7518560",
+    date: "2016.09.29",
+    media: "매일경제",
+    lang: "KOR",
+  },
+  {
+    title: "VIG파트너스 '버거킹' 2100억원에 어피니티에 매각",
+    link: "http://www.investchosun.com/site/data/html_dir/2016/02/18/2016021886024.html",
+    date: "2016.02.18",
+    media: "인베스트조선",
+    lang: "KOR",
+  },
+];
+const listEl = document.getElementById("list");
+const paginationEl = document.getElementById("pagination");
+
+let currentPage = 1;
+let rows = 10;
+
+function displayList(items, wrapper, rows_per_page, page) {
+  wrapper.innerHTML = "";
+  page--;
+
+  let start = rows_per_page * page;
+  let end = start + rows_per_page;
+  let paginatedItems = items.slice(start, end);
+
+  for (let i = 0; i < paginatedItems.length; i++) {
+    let item = `<article>
+			<a href="${paginatedItems[i].link}" target="_blank">
+				<h2 class="title-post">${paginatedItems[i].title}</h2>
+			</a>
+			<div class="info">
+				<div>
+					<span class="date">${paginatedItems[i].date}</span>
+					<span class="lang">${paginatedItems[i].lang}</span>
+				</div>
+				<div class="media">${paginatedItems[i].media}</div>
+			</div>
+		</article>`;
+
+    wrapper.insertAdjacentHTML("beforeend", item);
+  }
+}
+function setupPagination(items, wrapper, rows_per_page) {
+  wrapper.innerHTML = "";
+  // console.log(items);
+  let pageCount = Math.ceil(items.length / rows_per_page);
+  for (let i = 1; i < pageCount + 1; i++) {
+    let btn = paginationButton(i, items);
+    wrapper.appendChild(btn);
+  }
+}
+function paginationButton(page, items) {
+  let button = document.createElement("button");
+  button.innerText = page;
+
+  if (currentPage == page) button.classList.add("active");
+
+  button.addEventListener("click", () => {
+    currentPage = page;
+    displayList(items, listEl, rows, currentPage);
+
+    let currentBtn = document.querySelector(".pagenumbers button.active");
+    currentBtn.classList.remove("active");
+
+    button.classList.add("active");
+  });
+
+  return button;
+}
+
+displayList(listItem, listEl, rows, currentPage);
+setupPagination(listItem, paginationEl, rows);
