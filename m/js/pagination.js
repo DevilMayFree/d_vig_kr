e7986aa30362e14210d96178f3a2e4e641c080a2
Freedom <1358762936@qq.com>
2024-03-23 18:05:12 +0800

    init

---
+++ m/js/pagination.js
@@ -0,0 +1,383 @@
+const listItem = [
+	{
+		title: "South Korea’s VIG hits first close on Fund V, eyes exits",
+		link: "https://ionanalytics.com/insights/mergermarket/south-koreas-vig-hits-first-close-on-fund-v-eyes-exits-partners/",
+		date: "February 08, 2024",
+		media: "ION Analytics",
+		lang: "ENG",
+	},
+	{
+		title: "Korean PE VIG Partners soaks up shampoo brand Kundal’s growth",
+		link: "https://www.kedglobal.com/beauty-cosmetics/newsView/ked202311200008",
+		date: "November 20, 2023",
+		media: "The Korea Economic Daily",
+		lang: "ENG",
+	},
+	{
+		title: "Deal focus: VIG secures rare airline turnaround opportunity",
+		link: "https://www.avcj.com/avcj/official-record/3028505/deal-focus-vig-secures-rare-airline-turnaround-opportunity",
+		date: "January 17, 2023",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG acquires Korea's Eastar Jet",
+		link: "https://www.avcj.com/avcj/news/3028447/vig-acquires-koreas-eastar-jet",
+		date: "January 9, 2023",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title:
+			"[VIG Portfolio] Bonchon Crunches Through Q1 and Q2 of 2022 With Record Sales Numbers",
+		link: "https://finance.yahoo.com/news/bonchon-crunches-q1-q2-2022-181000197.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAADplEj-zd6omdM8p8w4bqzf9K-AgC59E3rKFOBnNFD-ldGd6VdoLDNNqaFKmqVrocBdPABKHQs9tmSyXRVYQHFPXPGd3t1rkn_-feeYLfm7sT7s6y5l4ZvkzVXHkBfNLv17Skn0lvglSdVy2DVDUUVDQfH0xYydsLZaBlzjXPoSE",
+		date: "July 20, 2022",
+		media: "Cision",
+		lang: "ENG",
+	},
+	{
+		title: "VIG buys Korean healthy beverage brand",
+		link: "https://www.avcj.com/avcj/news/3026737/vig-buys-korean-healthy-beverage-brand",
+		date: "April 27, 2022",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG Partners to acquire Korean tea-based drinks firm Teazen",
+		link: "https://www.asiatechdaily.com/vig-partners-to-acquire-korean-tea-based-drinks-firm-teazen/",
+		date: "April 26, 2022",
+		media: "AsiaTechDaily",
+		lang: "ENG",
+	},
+	{
+		title: "Korea's VIG makes first full exit from Fund III",
+		link: "https://www.avcj.com/avcj/news/3025828/koreas-vig-makes-first-full-exit-from-fund-iii",
+		date: "January 3, 2022",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG makes partial exit from Korea funeral services business",
+		link: "https://www.avcj.com/avcj/news/3024896/vig-makes-partial-exit-from-korea-funeral-services-business",
+		date: "November 6, 2021",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG Partners sells Foodist's logistics centers",
+		link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202110061523548960105842&svccode=00&page=1&sort=thebell_check_time",
+		date: "October 7, 2021",
+		media: "The Bell",
+		lang: "ENG",
+	},
+	{
+		title:
+			"VIG Partners joins hands with Mastern Partners as Preedlife continues to grow",
+		link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202109071818479520106360&svccode=00&page=1&sort=thebell_check_time",
+		date: "September 8, 2021",
+		media: "The Bell",
+		lang: "ENG",
+	},
+	{
+		title: "VIG buys Korea waste-to-energy business",
+		link: "https://www.avcj.com/avcj/news/3024865/vig-buys-korea-waste-to-energy-business",
+		date: "September 1, 2021",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title:
+			"Korean PEF VIG Partners invests about $129 mn in local waste treatment firm",
+		link: "https://pulsenews.co.kr/view.php?year=2021&no=844200",
+		date: "September 1, 2021",
+		media: "Maeil Business",
+		lang: "ENG",
+	},
+	{
+		title: "VIG Partners to recapitalize Bonchon",
+		link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202106171318137120103157&svccode=00&page=1&sort=thebell_check_time",
+		date: "June 18, 2021",
+		media: "The Bell",
+		lang: "ENG",
+	},
+	{
+		title: "VIG Partners acquires hair & body care product firm",
+		link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202105171305105240105084&svccode=00&page=1&sort=thebell_check_time",
+		date: "May 18, 2021",
+		media: "The Bell",
+		lang: "ENG",
+	},
+	{
+		title: "Korea's VIG buys The Skin Factory",
+		link: "https://www.avcj.com/avcj/news/3023846/koreas-vig-buys-the-skin-factory",
+		date: "May 13, 2021",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "Portfolio: VIG Partners and Joun Life/Preed Life",
+		link: "https://www.avcj.com/avcj/analysis/3022115/portfolio-vig-partners-and-joun-life-preed-life",
+		date: "December 10, 2020",
+		media: "AVCJ ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG Partners takes over Homenuri Mart",
+		link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=202006301456288560101730&svccode=00&page=1&sort=thebell_check_time",
+		date: "July 1, 2020",
+		media: "The Bell",
+		lang: "ENG",
+	},
+	{
+		title: "VIG makes second Korea funeral services acquisition",
+		link: "https://www.avcj.com/avcj/news/3018973/vig-makes-second-korea-funeral-services-acquisition",
+		date: "April 14, 2020",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "[VIG Fund focus] Korea's midcap buyout space comes into focus",
+		link: "https://www.avcj.com/avcj/official-record/3017902/fund-focus-korea-s-midcap-buyout-space-comes-into-focus",
+		date: "January 17, 2020",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG Partners raises $810m",
+		link: "https://www.privateequityinternational.com/vig-partners-raises-810m/?_gl=1*1m2tlt7*_up*MQ..*_ga*NDk4NTE0NzQ5LjE2NzI3MjAyNjI.*_ga_V7NRTBGL93*MTY3MjcyMDI2MS4xLjEuMTY3MjcyMDMzOC4wLjAuMA..",
+		date: "January 6, 2020",
+		media: "Private Equity International",
+		lang: "ENG",
+	},
+	{
+		title: "VIG reaches $810m final close on fourth Korean fund",
+		link: "https://www.avcj.com/avcj/news/3017750/vig-reaches-usd810m-final-close-on-fourth-korean-fund",
+		date: "January 6, 2020",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG Partners solidifies its position this year",
+		link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=201912131646337840107536&svccode=00&page=1&sort=thebell_check_time",
+		date: "December 16, 2019",
+		media: "The Bell",
+		lang: "ENG",
+	},
+	{
+		title: "VIG Partners buys Hanwha's food service operations",
+		link: "http://www.thebell.co.kr/free/content/ArticleView.asp?key=201911280100055810003437&svccode=00&page=1&sort=thebell_check_time",
+		date: "November 29, 2019",
+		media: "The Bell",
+		lang: "ENG",
+	},
+	{
+		title: "VIG acquires Korean education business",
+		link: "https://www.avcj.com/avcj/news/3016685/vig-acquires-korean-education-business",
+		date: "October 3, 2019",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG secures 3.2x return on Korea's HiParking",
+		link: "https://www.avcj.com/avcj/news/3016294/vig-secures-32x-return-on-koreas-hiparking",
+		date: "August 30, 2019",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG to make full exit from Korea's Samyang Optics",
+		link: "https://www.avcj.com/avcj/news/3015511/vig-to-make-full-exit-from-koreas-samyang-optics",
+		date: "June 27, 2019",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG hits $530m first close on fourth Korean fund",
+		link: "https://www.avcj.com/avcj/news/3014909/vig-hits-usd530m-first-close-on-fourth-korean-fund",
+		date: "May 13, 2019",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "[VIG Portfolio] Deal focus: Bonchon flies the coop",
+		link: "https://www.avcj.com/avcj/official-record/3012980/deal-focus-bonchon-flies-the-coop",
+		date: "December 13, 2018",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG buys global Korean fried chicken franchise",
+		link: "https://www.avcj.com/avcj/news/3012811/vig-buys-global-korean-fried-chicken-franchise",
+		date: "November 30, 2018",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG-owned Hi-Parking buys Wilson Parking Korea",
+		link: "https://www.avcj.com/avcj/news/3012774/vig-owned-hi-parking-buys-wilson-parking-korea",
+		date: "November 28, 2018",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG nets 2.1x return on Korea price comparison site deal",
+		link: "https://www.avcj.com/avcj/news/3010314/vig-nets-21x-return-on-korea-price-comparison-site-deal",
+		date: "June 8, 2018",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG invests $129m in Korean eyewear retailer",
+		link: "https://www.avcj.com/avcj/news/3010239/vig-invests-usd129m-in-korean-eyewear-retailer",
+		date: "June 5, 2018",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "Deal focus: VIG’s food supply chain upgrade",
+		link: "https://www.avcj.com/avcj/official-record/3008868/deal-focus-vig-s-food-supply-chain-upgrade",
+		date: "March 2, 2018",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG buys Korean food wholesaler for $69m",
+		link: "https://www.avcj.com/avcj/news/3008697/vig-buys-korean-food-wholesaler-for-usd69m",
+		date: "February 20, 2018",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG-owned Samyang targets up to $74m in Korea IPO",
+		link: "https://www.avcj.com/avcj/news/3004968/vig-owned-samyang-targets-up-to-usd74m-in-korea-ipo",
+		date: "May 17, 2017",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "PE and funeral services: Untapped eternity",
+		link: "https://www.avcj.com/avcj/analysis/3003919/pe-and-funeral-services-untapped-eternity",
+		date: "March 1, 2017",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "Fund focus: VIG steps up for Fund III",
+		link: "https://www.avcj.com/avcj/official-record/3003767/fund-focus-vig-steps-up-for-fund-iii",
+		date: "February 17, 2017",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG Partners successfully closes 3rd fund",
+		link: "http://www.koreatimes.co.kr/www/biz/2018/11/488_223992.html",
+		date: "February 15, 2017",
+		media: "The Korea Times",
+		lang: "ENG",
+	},
+	{
+		title: "VIG reaches $600m final close on third Korea fund",
+		link: "https://www.avcj.com/avcj/news/3003712/vig-reaches-usd600m-final-close-on-third-korea-fund",
+		date: "February 13, 2017",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "Portfolio: VIG Partners and Samyang Optics",
+		link: "https://www.avcj.com/avcj/analysis/3002308/portfolio-vig-partners-and-samyang-optics",
+		date: "October 19, 2016",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "Deal focus: VIG sees upside in sector that will never die",
+		link: "https://www.avcj.com/avcj/official-record/3002215/deal-focus-vig-sees-upside-in-sector-that-will-never-die",
+		date: "October 13, 2016",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "VIG invests in Korean funeral services provider",
+		link: "https://www.avcj.com/avcj/news/3002110/vig-invests-in-korean-funeral-services-provider",
+		date: "October 4, 2016",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title: "Korea's VIG Partners buys parking management business",
+		link: "https://www.avcj.com/avcj/news/99040/koreas-vig-partners-buys-parking-management-business",
+		date: "April 20, 2016",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+	{
+		title:
+			"VIG set for 2.5x return as Affinity agrees to buy Burger King Korea",
+		link: "https://www.avcj.com/avcj/news/50607/vig-set-for-25x-return-as-affinity-agrees-to-buy-burger-king-korea",
+		date: "February 19, 2016",
+		media: "AVCJ",
+		lang: "ENG",
+	},
+];
+const listEl = document.getElementById('list');
+const paginationEl = document.getElementById('pagination');
+
+let currentPage = 1;
+let rows = 10;
+
+function displayList(items, wrapper, rows_per_page, page) {
+	wrapper.innerHTML = "";
+	page--;
+
+	let start = rows_per_page * page;
+	let end = start + rows_per_page;
+	let paginatedItems = items.slice(start, end);
+
+	for (let i = 0; i < paginatedItems.length; i++) {
+		let item =
+			`<article>
+			<div class="info">
+				<div>
+					<span class="date">${paginatedItems[i].date}</span>
+					<span class="lang">${paginatedItems[i].lang}</span>
+				</div>
+				<div class="media">${paginatedItems[i].media}</div>
+			</div>
+			<a href="${paginatedItems[i].link}" target="_blank">
+				<p class="title-post" style="color: black">${paginatedItems[i].title}</p>
+			</a>
+		</article>`;
+
+		wrapper.insertAdjacentHTML('beforeend', item);
+	}
+}
+function setupPagination(items, wrapper, rows_per_page) {
+	wrapper.innerHTML = "";
+	// console.log(items);
+	let pageCount = Math.ceil(items.length / rows_per_page);
+	for (let i = 1; i < pageCount + 1; i++) {
+		let btn = paginationButton(i, items);
+		wrapper.appendChild(btn);
+	}
+}
+function paginationButton(page, items) {
+	let button = document.createElement('button');
+	button.innerText = page;
+
+	if (currentPage == page) button.classList.add('active');
+
+	button.addEventListener('click', () => {
+		currentPage = page;
+		displayList(items, listEl, rows, currentPage);
+
+		let currentBtn = document.querySelector('.pagenumbers button.active');
+		currentBtn.classList.remove('active');
+
+		button.classList.add('active');
+	});
+
+	return button;
+}
+
+displayList(listItem, listEl, rows, currentPage);
+setupPagination(listItem, paginationEl, rows);
